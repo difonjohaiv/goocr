317e6beed35df14e4232f871701e5d90191af008
difonjohaiv <edison0824@outlook.com>
2023-09-22 12:30:42 +0800

    0

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
+import cv2
 from rapidocr_onnxruntime import RapidOCR, VisRes
 
 engine = RapidOCR()
+vis = VisRes(font_path="font\\Roboto-Light.ttf")
 
-img_path = '01.png'
-result, elapse = engine(img_path)
-# print(result)
-print(elapse)
+img_path = 'images\\01.png'
+result, elapse_list = engine(img_path)
+boxes, txts, scores = list(zip(*result))
+res = vis(img_path, boxes, txts, scores)
+cv2.imwrite("vis_det_rec.png", res)
